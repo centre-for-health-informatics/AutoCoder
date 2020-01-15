7a16997b747b493b460ee9d63ceba637ae8fac55
Oscar Chen <oscar.y.chen@gmail.com>
2020-01-15 11:31:12 -0700

    added environment variable

--- Django/settings.py
+++ Django/settings.py
@@ -13,7 +13,7 @@ DEBUG = (os.environ['DJANGO_DEBUG'].lower() == 'true')
 if DEBUG:
     print("WARNING: Using development settings")
     CORS_ORIGIN_ALLOW_ALL = True
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = [os.environ['DJANGO_HOST_ADDRESS']]
 
 else:
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
